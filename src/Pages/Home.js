40520975b5f8aea94a99619019566a115e8b3852
Michelle <michellebaker1129@gmail.com>
2023-06-01 23:22:38 -0700

    style about page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -12,7 +12,7 @@ const Home=() => {
             </h2>
             <Image className="profile-pic" src={profilePic} rounded style={{ maxWidth: '94vw', margin: '0 2vw'}} />
 
-            <p>
+            <p style={{maxWidth: "1000px"}}>
                 I am a full stack web developer with a passion for learning and creating. I have a background in the arts and in retail, and I am excited to bring my creativity and problem solving skills to the world of web development. I am currently working on a certificate in full stack web development from the University of California, Berkeley. I am proficient in HTML, CSS, JavaScript, Node.js, Express.js, MySQL, MongoDB, and React.js. I am a team player and I am excited to work with others to create something amazing.
             </p>
         </section>
